0556cb67c3fdefc0ae6d12718603c2474b334299
VaneGarciaYara <vanegarciay@gmail.com>
2017-06-11 12:15:24 -0400

    agregue tooltip botom

--- js/main.js
+++ js/main.js
@@ -3,3 +3,7 @@ var barra = document.getElementById("barra");
 barra.addEventListener("click",function(){
     document.getElementById("menu-responsive").classList.toggle("invisible");
 });
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
